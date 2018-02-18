22d34dde1c332cb7b1250be445f59d9320e4e173
Fahim Ahmed <fahim@grappetite.com>
2018-02-19 01:21:43 +0500

    setup project

--- index.php
+++ index.php
@@ -84,6 +84,8 @@ switch (ENVIRONMENT)
 	break;
 
 	case 'testing':
+        error_reporting(-1);
+        ini_set('display_errors', 1);
 	case 'production':
 		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
