ec676c1ba8b04a48ea5ff9a22859e126a1710605
Fahim Ahmed <fahim@grappetite.com>
2018-02-21 23:06:24 +0500

    setup project

--- application/views/footer.php
+++ application/views/footer.php
@@ -53,5 +53,10 @@
 
 
 
+
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5a8c4ceecbac6177"></script>
+
+
 </body>
 </html>
