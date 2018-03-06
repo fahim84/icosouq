758b6996d57e9968d7502a66d6b1475c703d52e8
Fahim Ahmed <fahim@grappetite.com>
2018-03-06 23:20:25 +0500

    setup project

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('role_model','user_model','general_model','ico_model');
+$autoload['model'] = array('role_model','user_model','general_model','ico_model','article_model');
