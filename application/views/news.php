e4e4989fca837a05413e327737e2717044c60e43
Fahim Ahmed <fahim@grappetite.com>
2018-05-01 03:42:41 +0500

    bug fixes

--- application/views/news.php
+++ application/views/news.php
@@ -8,7 +8,7 @@
 
 
         <div class="col-sm-12">
-            <table width="100%">
+            <!--<table width="100%">-->
             <?php foreach($rows->result() as $row)
             {
             $image_url = $row->image == '' ? base_url().'uploads/articles/placeholder.png' : base_url().'uploads/articles/'.$row->image;
@@ -16,49 +16,48 @@
 
             ?>
 
-                <tr>
+                <!--<tr>
                     <td valign="top" width="200">
-                        <a href="<?php echo base_url(); ?>welcome/newsdetail?id=<?php echo $row->article_id; ?>" >
-                            <img src="<?php echo $image_url; ?>" alt="<?php echo $row->article; ?>" width="200">
+                        <a href="<?php /*echo base_url(); */?>welcome/newsdetail?id=<?php /*echo $row->article_id; */?>" >
+                            <img src="<?php /*echo $image_url; */?>" alt="<?php /*echo $row->article; */?>" width="200">
                         </a>
                     </td>
                     <td>&nbsp;&nbsp;&nbsp;</td>
                     <td valign="top">
                         <h2 class="header">
-                            <a href="<?php echo base_url(); ?>welcome/newsdetail?id=<?php echo $row->article_id; ?>"> <?php echo $row->article; ?> </a>
+                            <a href="<?php /*echo base_url(); */?>welcome/newsdetail?id=<?php /*echo $row->article_id; */?>"> <?php /*echo $row->article; */?> </a>
                         </h2>
                         <div class="info">
-                            <span class="date"> <?php echo date("j M Y",strtotime($row->post_date)); ?> </span>
+                            <span class="date"> <?php /*echo date("j M Y",strtotime($row->post_date)); */?> </span>
                         </div>
                         <p class="text">
-                            <?php echo character_limiter($row->description,400); ?>
+                            <?php /*echo character_limiter($row->description,400); */?>
                         </p>
                     </td>
-                </tr>
-            <!--<div class="row result">
-                <figure class="col-sm-4">
-                    <div class="image">
-                        <a href="<?php /*echo base_url(); */?>welcome/newsdetail?id=<?php /*echo $row->article_id; */?>" >
-                                <img src="<?php /*echo $image_url; */?>" alt="<?php /*echo $row->article; */?>" width="200">
-                                <p class="sponsored badge badge-default" style="display: block;">News</p>
-
+                </tr>-->
+            <div class="row result">
+                <figure class="col-sm-3">
+                    <div class="">
+                        <a href="<?php echo base_url(); ?>welcome/newsdetail?id=<?php echo $row->article_id; ?>" >
+                                <img src="<?php echo $image_url; ?>" alt="<?php echo $row->article; ?>" width="270">
                         </a>
                     </div>
                 </figure>
-                <figure class="col-sm-8">
+                <figure class="col-sm-9">
                     <h2 class="header">
-                        <a href="<?php /*echo base_url(); */?>welcome/newsdetail?id=<?php /*echo $row->article_id; */?>"> <?php /*echo $row->article; */?> </a>
+                        <a href="<?php echo base_url(); ?>welcome/newsdetail?id=<?php echo $row->article_id; ?>"> <?php echo $row->article; ?> </a>
                     </h2>
                     <div class="info">
-                        <span class="date"> <?php /*echo date("j M Y",strtotime($row->post_date)); */?> </span>
+                        <span class="date"> <?php echo date("j M Y",strtotime($row->post_date)); ?> </span>
                     </div>
                     <p class="text">
-                        <a href="<?php /*echo base_url(); */?>welcome/newsdetail?id=<?php /*echo $row->article_id; */?>"> <?php /*echo character_limiter($row->description,200); */?> </a>
+                        <?php echo $row->description; ?>
                     </p>
                 </figure>
-            </div>-->
+            </div>
+                <div class="row">&nbsp;</div>
             <?php } ?>
-            </table>
+            <!--</table>-->
             <?php echo $pagination_links; ?>
         </div>
     </div>
@@ -66,7 +65,7 @@
 
     <div class="row">
         <div class="col-xs-12">
-            <table width="100%">
+            <!--<table width="100%">-->
             <?php
             $url = "https://cryptocurrencynews.com/category/basic-materials/daily-news/bitcoin-news/feed/";
             $string = file_get_contents($url);
@@ -86,36 +85,58 @@
 
                 ?>
 
-                <tr>
+                <!--<tr>
                     <td valign="top" width="200">
-                        <a href="<?php echo $item->link; ?>" target="_blank" >
-                            <img src="<?php echo $image_url; ?>" alt="<?php echo $item->title; ?>" width="200">
+                        <a href="<?php /*echo $item->link; */?>" target="_blank" >
+                            <img src="<?php /*echo $image_url; */?>" alt="<?php /*echo $item->title; */?>" width="200">
                         </a>
                     </td>
                     <td>&nbsp;&nbsp;&nbsp;</td>
                     <td valign="top">
                         <h2 class="header">
-                            <a href="<?php echo $item->link; ?>" target="_blank"> <?php echo $item->title; ?> </a>
+                            <a href="<?php /*echo $item->link; */?>" target="_blank"> <?php /*echo $item->title; */?> </a>
                         </h2>
                         <div class="info">
-                            <span class="date"> <?php echo date("j M Y",strtotime($item->pubDate)); ?> </span>
+                            <span class="date"> <?php /*echo date("j M Y",strtotime($item->pubDate)); */?> </span>
                         </div>
                         <p class="text">
-                             <?php echo $desc; ?>
+                             <?php /*echo $desc; */?>
                         </p>
                     </td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
                     <td>&nbsp;</td>
-                </tr>
+                </tr>-->
 
+                <div class="row result">
+                    <figure class="col-sm-3">
+                        <div class="">
+                            <a href="<?php echo $item->link; ?>" target="_blank" >
+                                <img src="<?php echo $image_url; ?>" alt="<?php echo $item->title; ?>" width="270">
+
+                            </a>
+                        </div>
+                    </figure>
+                    <figure class="col-sm-9">
+                        <h2 class="header">
+                            <a href="<?php echo $item->link; ?>" target="_blank" > <?php echo $item->title; ?> </a>
+                        </h2>
+                        <div class="info">
+                            <span class="date"> <?php echo date("j M Y",strtotime($item->pubDate)); ?> </span>
+                        </div>
+                        <p class="text">
+                            <?php echo $desc; ?>
+                        </p>
+                    </figure>
+                </div>
+                <div class="row">&nbsp;</div>
                 <?php
             }
 
 
             ?>
-            </table>
+            <!--</table>-->
 
         </div>
         <?php //echo $pagination_links; ?>
