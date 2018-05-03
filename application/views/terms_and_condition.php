fe23b1b226e792b537fc3b2afd54e7b80443209d
Fahim Ahmed <fahim@grappetite.com>
2018-05-04 01:27:59 +0500

    bug fixes

--- application/views/terms_and_condition.php
+++ application/views/terms_and_condition.php
@@ -4,12 +4,12 @@
     <div class="col-xs-12">
 
         <div >
-            <h1>Terms and Conditions</h1>
+            <h2>Terms and Conditions</h2>
             <p>
                 Please read the Terms and Conditions (“Terms”, “Terms and Conditions”) before using the ICOSouq.com website (the “Service”, “us”, “we”, or “our”).
             </p>
 
-            <h1>Disclaimers</h1>
+            <h2>Disclaimers</h2>
             <p>
                 All ratings published on ICOSouq serve informational purposes only. ICOSouq does not provide investment forecast, recommendations or any consulting for that matter. Thus, neither ICOSouq, nor the experts can be hold responsible for the visitors’ investment decisions.
             </p>
@@ -20,7 +20,7 @@
                 ICO ratings published on ICOSouq solely represent the opinions of the experts providing it. The experts conduct the ratings based on their professional experience, publicly available information (whitepapers, roadmaps, interviews, reports) and possibly dialogues with the ICO project management team*. And while we hold ourselves and our experts accountable to high professional standards, we keep the right to have human errors and misjudgement based on our and our experts’ personal circumstances.
             </p>
 
-            <h1>Payments</h1>
+            <h2>Payments</h2>
             <p>
                 ICOSouq does not solicit or accept money/any other mean, holding a value, from anybody to submit an ICO for a review. Thus, ICOSouq highly advises projects, that are willing to have their ICOs reviewed on our website, not to pay any money to third parties, claiming a publication guarantee.
             </p>
@@ -28,7 +28,7 @@
                 Submitting an ICO for review at ICOSouq, does not guarantee a publication. We reserve the right to refuse or cancel your submission for review.
             </p>
 
-            <h1>Receiving communication</h1>
+            <h2>Receiving communication</h2>
             <p>
                 By becoming an expert to review ICOs, you agree to receive the communication materials we may send.
                 By signing up for the newsletter, you agree to receiving newsletters.
@@ -38,7 +38,7 @@
                 The service may employ the use of third party services for the purpose of facilitating payment and the completion of Purchases. By submitting your information, you grant us the right to provide the information to these third parties.
             </p>
 
-            <h1>Accounts</h1>
+            <h2>Accounts</h2>
             <p>
                 When signing up for an expert account, you agree that you are above the age of 18 and have provided accurate information, and you are not using any other identity but yours. It is your responsibility to keep the confidentiality of your account and password.
             </p>
@@ -47,7 +47,7 @@
                 We reserve the right to terminate accounts, remove or edit content in our sole discretion.
             </p>
 
-            <h1>Intellectual Property and Copyright</h1>
+            <h2>Intellectual Property and Copyright</h2>
             <p>
                 Most of the content on ICO pages are owned by the ICO holders. All other content on the website is the intellectual right of ICOSouq, unless otherwise specified.
             </p>
