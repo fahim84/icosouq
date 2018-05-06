97745e83dc648b28d969b82e1e21b9fb075f40bc
Fahim Ahmed <fahim@grappetite.com>
2018-05-07 02:45:17 +0500

    Final deployment

--- application/controllers/Backup.php
+++ application/controllers/Backup.php
@@ -61,7 +61,7 @@ class Backup extends CI_Controller {
 		$this->email->subject(SYSTEM_NAME." database backup");
 		$this->email->message($message);
 		
-		$this->email->attach("./$backup_file");
+		//$this->email->attach("./$backup_file");
 		$this->email->send();
 	}
 	
@@ -115,7 +115,7 @@ class Backup extends CI_Controller {
         $this->email->subject(SYSTEM_NAME." zip backup");
         $this->email->message($message);
 
-        $this->email->attach("./$backup_file");
+        //$this->email->attach("./$backup_file");
         $this->email->send();
 	}
 	
