866be23c164369a53653c8c77ac95a4ddb10f549
Fahim Ahmed <fahim@grappetite.com>
2018-07-12 17:52:04 +0500

    Bug fixes

--- application/views/icobench_ongoing.php
+++ application/views/icobench_ongoing.php
@@ -22,6 +22,87 @@
     <div class="row">
         <div class="col-xs-12">
         <table class="main-ico-table">
+
+
+            <?php foreach($rows->result() as $row)
+            {
+                $end_date = $row->icoEnd;
+                $date = DateTime::createFromFormat("Y-m-d H:i:s",$end_date);
+                $date2 = new DateTime();
+
+                $days = $date2->diff($date)->format("%a");
+                $hours = $date2->diff($date)->format("%H");
+                $minutes = $date2->diff($date)->format("%i");
+
+            if($row->featured){
+                ?>
+                <tr>
+                    <td>
+                        <div class="row" style='position:relative;'>
+
+                            <?php if($row->featured){ ?>
+                                <a href='<?php echo base_url(); ?>welcome/detail?id=<?php echo $row->id; ?>'><img src='<?php echo base_url(); ?>assets/icosouq/images/gold-ico-light.png' class='sponsored-label' style='position:absolute;top:0px;right:15px;'></a>
+                            <?php } ?>
+                            <div class='db logo-div col-xs-12 col-sm-3 col-md-1 col-xl-2'>
+                                <div style='margin:5px 15px;'>
+                                    <a title="<?php echo $row->name; ?>" href='<?php echo base_url(); ?>welcome/detail?id=<?php echo $row->id; ?>' style='text-decoration: none;'>
+                                        <img src='<?php echo $row->logo; ?>'  alt='<?php echo $row->name; ?>'>
+
+                                    </a>
+                                </div>
+                            </div>
+
+
+                            <div class='db description-div  col-xs-12 col-sm-3 col-md-3 col-lg-4 col-xl-2'>
+                                <div class="aligndesc">
+                                    <div class='project-time' style="font-size: 16px;" ><?php echo $row->name; ?></div>
+                                    <p class='project-info'><?php echo $row->desc; ?></p>
+                                </div>
+                            </div>
+
+
+                            <div class='db countdown-div hidden-xs col-xs-12 col-sm-3 col-md-3 col-lg-2 col-xl-2'>
+                                <div style='margin-top:5px;'>
+                                    <p style="font-size:12px;margin:0px;"><!--<span style="color:#ffae00;font-weight:bold;">PRESALE</span>-->ENDS IN:</p>
+                                    <div class='time-unit-div'>
+                                        <p class='project-time'><?php echo $days; ?></p>
+                                        <p class='small-time'>Days</p>
+                                    </div>
+                                    <div class='time-unit-div'>
+                                        <p class='project-time'><?php echo $hours; ?></p>
+                                        <p class='small-time'>Hours</p>
+                                    </div>
+                                    <div class='time-unit-div seconds'>
+                                        <p class='project-time'><?php echo $minutes; ?></p>
+                                        <p class='small-time'>Minutes</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="db progress-div col-xs-12 col-sm-4 col-md-2 col-lg-2 col-xl-2">
+
+
+                                <p class='project-time' style='margin:26px;font-size: 22px;' align="center"><?php echo $row->ico_souq_rate; ?></p>
+
+                            </div>
+
+                            <div class='db button-div col-xs-12 col-sm-2 col-md-2 col-lg-2 col-xl-2'>
+                                <div style='text-align: center;'>
+                                    <a href='<?php echo base_url(); ?>welcome/detail?id=<?php echo $row->id; ?>' title='<?php echo $row->name; ?> Details'><div class='progress-ico'>ICO Details</div></a>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </td>
+                </tr>
+            <?php }} ?>
+
+
+
+
+
+
             <?php
             $query_params['limit'] = 50;
             $query_params['offset'] = 0;
@@ -115,7 +196,7 @@
                 $days = $date2->diff($date)->format("%a");
                 $hours = $date2->diff($date)->format("%H");
                 $minutes = $date2->diff($date)->format("%i");
-
+            if($row->featured == '0'){
                 ?>
                 <tr>
                     <td>
@@ -177,7 +258,7 @@
 
                     </td>
                 </tr>
-            <?php } ?>
+            <?php }} ?>
 
         </table>
         </div>
