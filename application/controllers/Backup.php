ac5b54051215bbbbd37d8fb1167a6210c748d0e9
Fahim Ahmed <fahim@grappetite.com>
2018-07-14 16:21:35 +0500

    Final deployment

--- application/controllers/Backup.php
+++ application/controllers/Backup.php
@@ -72,6 +72,7 @@ class Backup extends CI_Controller {
         $skip_files_and_directories[] = 'ci_sessions';
         $skip_files_and_directories[] = 'error.log';
         $skip_files_and_directories[] = 'uploads';
+        $skip_files_and_directories[] = 'vendor';
         $skip_files_and_directories[] = $backup_file;
         if ($handle = opendir('.')) {
             while (false !== ($entry = readdir($handle))) {
